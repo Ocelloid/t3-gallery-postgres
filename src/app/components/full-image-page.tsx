e6ea93c8c811e67b56c91bf9598e801f8cb9eb04
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-05-17 18:05:00 +0500

    modal cleanup

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -3,5 +3,27 @@ import Image from "next/image";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <Image src={image.url} alt="gallery image" width={420} height={420} />;
+  return (
+    // <div className="relative size-full">
+    // <Image
+    //   src={image.url}
+    //   fill
+    //   priority
+    //   className="object-contain"
+    //   alt={image.name}
+    // />
+    // </div>
+    <div className="flex size-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img
+          src={image.url}
+          className="flex-shrink object-contain"
+          alt={image.name}
+        />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
