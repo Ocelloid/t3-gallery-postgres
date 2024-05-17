9ea6a69a0295e77dad96bbaab1701993060d3bef
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-05-17 21:39:52 +0500

    setup chadcn/ui

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -2,13 +2,9 @@
 
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
 import SimpleUploadButton from "./simple-upload-button";
 
 export default function TopNav() {
-  const router = useRouter();
-
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <Link href="/">Gallery</Link>
