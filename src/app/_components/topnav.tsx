6ca04d4fc0f2c11aeda053b155e34e9abb56cad3
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-05-17 21:06:48 +0500

    finished the modal

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
 
@@ -9,7 +10,7 @@ export default function TopNav() {
 
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+      <Link href="/">Gallery</Link>
       <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -1,28 +1,36 @@
 import { getImage } from "~/server/db/queries";
 import Image from "next/image";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const user = await clerkClient.users.getUser(image.userId);
   return (
-    // <div className="relative size-full">
-    // <Image
-    //   src={image.url}
-    //   fill
-    //   priority
-    //   className="object-contain"
-    //   alt={image.name}
-    // />
-    // </div>
-    <div className="flex size-full min-w-0">
-      <div className="flex flex-shrink items-center justify-center">
-        <img
+    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+      <div className="relative size-full">
+        <Image
           src={image.url}
-          className="flex-shrink object-contain"
+          fill
+          priority
+          className="object-contain"
           alt={image.name}
         />
       </div>
-      <div className="flex w-48 flex-shrink-0 flex-col border-l">
-        <div className="text-xl font-bold">{image.name}</div>
+      {/* <img
+          src={image.url}
+          className="flex-shrink object-contain"
+          alt={image.name}
+        /> */}
+      <div className="flex h-full w-48 flex-shrink-0 flex-grow flex-col border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+        <div className="flex flex-col p-2">
+          <span className="text-sm">Uploaded by:</span>
+          <span>{user.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span className="text-sm">Created on:</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
